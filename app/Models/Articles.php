3f9eaf3086e4c0b25601ff7d4da4979cb9621c53
banafsheh <banafsheh.azizi91@gmail.com>
2023-09-06 16:08:13 +0330

    add models

---
+++ app/Models/Articles.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Articles extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'title',
+        'summary',
+        'author_id',
+        'status_id',
+        'priority_id',
+        'published_at'
+    ];
+
+    protected $table = 'articles';
+}
