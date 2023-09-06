b6a375e11ccb9f1697548d5234e700e61680f543
banafsheh <banafsheh.azizi91@gmail.com>
2023-09-06 18:33:56 +0330

    add routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,24 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => '/v2'], function () {
+    Route::post('/register', [App\Http\Controllers\Users\UserController::class, 'register'])->name('register')->middleware(['check_user_event:3']);
+    Route::post('/login', [App\Http\Controllers\Users\AuthController::class, 'login'])->name('login');
+
+    Route::group(['middleware' => ['auth:api', 'check_access_token']], function () {
+        Route::get('/logout', [App\Http\Controllers\Users\AuthController::class, 'logout'])->name('logout');
+
+        Route::group(['prefix' => '/articles'], function () {
+            Route::get('/', [App\Http\Controllers\Articles\ArticleController::class, 'list'])->name('show_articles');
+            Route::get('/{article_id}', [App\Http\Controllers\Articles\ArticleController::class, 'show'])->name('show_article');
+            Route::post('/', [App\Http\Controllers\Articles\ArticleController::class, 'insert'])->name('insert_article')->middleware(['check_user_event:1']);
+            Route::delete('/{article_id}', [App\Http\Controllers\Articles\ArticleController::class, 'delete'])->name('delete_article');
+            Route::put('/{article_id}', [App\Http\Controllers\Articles\ArticleController::class, 'update'])->name('update_article');
+        });
+
+        Route::group(['prefix' => '/files'], function () {
+            Route::post('/', [App\Http\Controllers\Files\FileController::class, 'insert'])->name('insert_file')->middleware(['check_user_event:2']);
+        });
+    });
 });
